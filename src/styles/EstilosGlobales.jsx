5e759f27a685138da9b93fcc9f06c7406e6353db
Diego Aldao <diego_ind@live.com.ar>
2022-04-05 15:11:23 -0300

    añadidos estilos globales

--- src/styles/EstilosGlobales.jsx
+++ src/styles/EstilosGlobales.jsx
@@ -5,6 +5,17 @@ import Variables from "./Variables";
 const EstilosGlobales = createGlobalStyle`
     ${Fonts};
     ${Variables};
+
+    html{
+        width:100%;
+        scroll-behavior: smooth;
+    }
+    
+    *{
+        margin: 0px;
+        padding: 0px;
+        box-sizing: border-box;
+    }
 `;
 
 export default EstilosGlobales;
