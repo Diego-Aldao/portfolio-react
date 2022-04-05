5326c2c5b758f5d3ecb56f651312a08ea46fdeb8
Diego Aldao <diego_ind@live.com.ar>
2022-04-05 15:49:51 -0300

    añadidos estilos globales de los headings, imagenes y parrafos

--- src/styles/EstilosGlobales.jsx
+++ src/styles/EstilosGlobales.jsx
@@ -3,19 +3,50 @@ import Fonts from "./Fonts";
 import Variables from "./Variables";
 
 const EstilosGlobales = createGlobalStyle`
-    ${Fonts};
-    ${Variables};
+  ${Fonts};
+  ${Variables};
 
-    html{
-        width:100%;
-        scroll-behavior: smooth;
-    }
-    
-    *{
-        margin: 0px;
-        padding: 0px;
-        box-sizing: border-box;
-    }
+  body {
+    background: var(--navy);
+  }
+
+  html {
+    width:100%;
+    scroll-behavior: smooth;
+  }
+
+  * {
+  margin: 0px;
+  padding: 0px;
+  box-sizing: border-box;
+  }
+
+  h1,
+  h2,
+  h3,
+  h4,
+  h5,
+  h6 {
+   margin: 0 0 10px 0;
+   font-weight: 600;
+   color: var(--lightest-slate);
+   line-height: 1.1;
+  }
+  
+  img,
+  svg {
+   width: 100%;
+   max-width: 100%;
+   vertical-align: middle;
+  }
+  
+  p {
+   margin: 0 0 15px 0;
+   &:last-child,
+   &:last-of-type {
+     margin: 0;
+   }
+  }
 `;
 
 export default EstilosGlobales;
