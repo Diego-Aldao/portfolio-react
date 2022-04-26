a6633da0fe04a9c3bc29d4f9f7ebc0a885639736
Diego Aldao <diego_ind@live.com.ar>
2022-04-26 17:59:08 -0300

    añadidas las routas con react router

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,17 @@
 import PaginaIndex from "./pages";
+import PaginaArchivo from "./pages/archivo";
 import EstilosGlobales from "./styles/EstilosGlobales";
+import { Routes, Route, HashRouter } from "react-router-dom";
 
 function App() {
   return (
-    <>
+    <HashRouter>
       <EstilosGlobales />
-      <PaginaIndex />
-    </>
+      <Routes>
+        <Route path="/" element={<PaginaIndex />}></Route>
+        <Route path="/archivo" element={<PaginaArchivo />}></Route>
+      </Routes>
+    </HashRouter>
   );
 }
 
