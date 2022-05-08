919815457e3d8040ae2e956414d7b47a151bdde5
Diego Aldao <diego_ind@live.com.ar>
2022-05-08 12:52:08 -0300

    añadida la pagina 404

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import PaginaIndex from "./pages";
 import PaginaArchivo from "./pages/archivo";
 import EstilosGlobales from "./styles/EstilosGlobales";
 import { Routes, Route, HashRouter } from "react-router-dom";
+import Pagina404 from "./pages/404";
 
 function App() {
   return (
@@ -10,6 +11,7 @@ function App() {
       <Routes>
         <Route path="/" element={<PaginaIndex />}></Route>
         <Route path="/archivo" element={<PaginaArchivo />}></Route>
+        <Route path="/*" element={<Pagina404 />} />
       </Routes>
     </HashRouter>
   );
---
+++ src/pages/404.jsx
@@ -0,0 +1,45 @@
+import React from "react";
+import styled from "styled-components";
+import Layout from "../components/Layout";
+import { Link } from "react-router-dom";
+
+const Main = styled.main`
+  width: 100%;
+  min-height: 600px;
+  height: 100vh;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  flex-direction: column;
+  h1 {
+    font-size: clamp(100px, 25vw, 180px);
+    color: var(--color-principal);
+  }
+  h2 {
+    font-size: clamp(26px, 5vw, 42px);
+    text-transform: capitalize;
+    text-align: center;
+    font-weight: 400;
+  }
+`;
+const MiLink = styled(Link)`
+  padding: 15px 20px;
+  border: 1px solid var(--color-principal);
+  color: var(--color-principal);
+  margin-top: 30px;
+  text-transform: capitalize;
+`;
+
+const Pagina404 = () => {
+  return (
+    <Layout>
+      <Main>
+        <h1>404</h1>
+        <h2>pagina no encontrada</h2>
+        <MiLink to="/">ir al inicio</MiLink>
+      </Main>
+    </Layout>
+  );
+};
+
+export default Pagina404;
