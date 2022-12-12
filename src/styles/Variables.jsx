71396d130357628905880ff80796c92e0d706bf7
Diego Aldao <diego_ind@live.com.ar>
2022-12-12 14:42:20 -0300

    modificados los nombres de las variables y eliminados los colores innecesarios

--- src/styles/Variables.jsx
+++ src/styles/Variables.jsx
@@ -2,17 +2,12 @@ import { css } from "styled-components";
 
 const Variables = css`
   :root {
-    --color-principal: #ff848e;
-    --dark-navy: #020c1b;
-    --navy: #0a192f;
-    --light-navy: #112240;
-    --lightest-navy: #233554;
-    --navy-shadow: #020c1bb3;
-    --dark-slate: #495670;
-    --slate: #8892b0;
-    --light-slate: #a8b2d1;
-    --lightest-slate: #ccd6f6;
-    --white: #e6f1ff;
+    --color-principal: #fe6187;
+    --color-bg-principal: #202224;
+    --color-bg-secundario: #292d3e;
+    --color-fuente-secundario: #8892b0;
+    --color-fuente-terciario: #a8b2d1;
+    --color-fuente-principal: #ccd6f6;
 
     --fuente-mono: "SF Mono", monospace;
     --fuente-sans: "Montserrat", sans-serif;
