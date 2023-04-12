dca3ae82d397a06cf30419b92552a294d17dad49
Diego Aldao <diego_ind@live.com.ar>
2023-04-11 21:01:25 -0300

    reemplazado hash router por browser router

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,19 @@
 import PaginaIndex from "./pages";
 import PaginaArchivo from "./pages/archivo";
 import EstilosGlobales from "./styles/EstilosGlobales";
-import { Routes, Route, HashRouter } from "react-router-dom";
+import { Routes, Route, BrowserRouter } from "react-router-dom";
 import Pagina404 from "./pages/404";
 
 function App() {
   return (
-    <HashRouter>
+    <BrowserRouter>
       <EstilosGlobales />
       <Routes>
         <Route path="/" element={<PaginaIndex />}></Route>
         <Route path="/archivo" element={<PaginaArchivo />}></Route>
         <Route path="/*" element={<Pagina404 />} />
       </Routes>
-    </HashRouter>
+    </BrowserRouter>
   );
 }
 
