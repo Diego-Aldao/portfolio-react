fc1cf8c74291425bcd0c223bf44295f703379cc6
Diego Aldao <diego_ind@live.com.ar>
2023-04-11 21:03:58 -0300

    añadida una linea animada con react-spring y cambiado el children por props de titulo y descripcion

--- src/components/HeaderSecciones.jsx
+++ src/components/HeaderSecciones.jsx
@@ -1,41 +1,62 @@
 import React from "react";
 import styled from "styled-components";
+import { useInView, animated } from "@react-spring/web";
 
 const Contenedor = styled.header`
   margin-bottom: 40px;
-  span {
-    margin-right: 5px;
-    width: 50px;
-    height: 50px;
-    display: inline-block;
+  div {
+    position: relative;
+    overflow: hidden;
   }
   h2 {
     font-size: clamp(24px, 4vw, 38px);
     text-transform: capitalize;
     margin: 0px;
     position: relative;
-  }
-  h2:after {
-    content: "";
-    position: absolute;
-    right: 0px;
-    top: calc(50% - 1px);
-    width: 55%;
-    height: 1px;
-    background: var(--color-fuente-principal);
+    background: var(--color-bg-principal);
+    display: inline-block;
+    padding-right: 60px;
   }
   .descripcion-section {
     margin-top: 20px;
   }
-  @media (min-width: 580px) {
-    h2:after {
-      width: 75%;
-    }
-  }
 `;
 
-const Header = ({ children }) => {
-  return <Contenedor>{children}</Contenedor>;
+const Linea = styled(animated.span)`
+  position: absolute;
+  bottom: calc(50% - 3px);
+  right: 0px;
+  height: 2px;
+  background: var(--color-fuente-principal);
+  z-index: -1;
+`;
+
+const Header = ({ titulo, descripcion }) => {
+  const [ref, springs] = useInView(
+    () => ({
+      from: {
+        width: `${0}%`,
+        opacity: 0.2,
+      },
+      to: {
+        width: `${80}%`,
+        opacity: 1,
+      },
+      config: { mass: 1, tension: 35, friction: 10 },
+    }),
+    {
+      rootMargin: "-14% 0%",
+    }
+  );
+  return (
+    <Contenedor>
+      <div>
+        <h2>{titulo}</h2>
+        <Linea ref={ref} style={springs}></Linea>
+      </div>
+      {descripcion && <p className="descripcion-section">{descripcion}</p>}
+    </Contenedor>
+  );
 };
 
 export default Header;
