8c04acfa4c8e1de88fbaed523d1222811195ccae
Diego Aldao <diego_ind@live.com.ar>
2023-04-13 20:57:42 -0300

    modificados los colores, añadido un set de colores para el modo nocturno y cambiadas las fuentes

--- src/styles/Variables.jsx
+++ src/styles/Variables.jsx
@@ -2,16 +2,23 @@ import { css } from "styled-components";
 
 const Variables = css`
   :root {
-    --color-principal: #fe6187;
-    --color-bg-principal: #202224;
-    --color-bg-secundario: #292d3e;
-    --color-fuente-secundario: #8892b0;
-    --color-fuente-terciario: #a8b2d1;
-    --color-fuente-principal: #ccd6f6;
-
-    --fuente-mono: "SF Mono", monospace;
-    --fuente-sans: "Montserrat", sans-serif;
-
+    --color-principal: #8035e5;
+    --color-bg-principal: #e6e6e6;
+    --color-bg-secundario: #dddddd;
+    --color-fuente-principal: #001d28;
+    --color-fuente-secundario: #555555;
+    --color-fuente-terciario: #747474;
+  }
+  .dark-mode {
+    --color-bg-principal: #001d28;
+    --color-bg-secundario: #333333;
+    --color-fuente-principal: #e6e6e6;
+    --color-fuente-secundario: #cacaca;
+    --color-fuente-terciario: #bdbdbd;
+  }
+  :root {
+    --fuente-mono: "Azeret Mono", monospace;
+    --fuente-sans: "Poppins", sans-serif;
     --easing: cubic-bezier(0.645, 0.045, 0.355, 1);
     --transition: all 0.25s cubic-bezier(0.645, 0.045, 0.355, 1);
   }
