9781c09500abb6487ef89af2a9e0037b89bb46db
Diego Aldao <diego_ind@live.com.ar>
2023-04-16 10:51:37 -0300

    añadido el context para el modo oscuro

--- src/App.jsx
+++ src/App.jsx
@@ -3,17 +3,20 @@ import PaginaArchivo from "./pages/archivo";
 import EstilosGlobales from "./styles/EstilosGlobales";
 import { Routes, Route, BrowserRouter } from "react-router-dom";
 import Pagina404 from "./pages/404";
+import { DarkModeContextProvider } from "./context/DarkModeContext";
 
 function App() {
   return (
-    <BrowserRouter>
-      <EstilosGlobales />
-      <Routes>
-        <Route path="/" element={<PaginaIndex />}></Route>
-        <Route path="/archivo" element={<PaginaArchivo />}></Route>
-        <Route path="/*" element={<Pagina404 />} />
-      </Routes>
-    </BrowserRouter>
+    <DarkModeContextProvider>
+      <BrowserRouter>
+        <EstilosGlobales />
+        <Routes>
+          <Route path="/" element={<PaginaIndex />}></Route>
+          <Route path="/archivo" element={<PaginaArchivo />}></Route>
+          <Route path="/*" element={<Pagina404 />} />
+        </Routes>
+      </BrowserRouter>
+    </DarkModeContextProvider>
   );
 }
 
