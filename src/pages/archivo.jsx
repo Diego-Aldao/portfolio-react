f2cb7bf13e048635280d896254794803cd873569
Diego Aldao <diego_ind@live.com.ar>
2023-04-16 12:57:13 -0300

    eliminado un useState innecesario

--- src/pages/archivo.jsx
+++ src/pages/archivo.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React from "react";
 import Layout from "../layouts/Layout";
 import HeaderArchivo from "../components/paginaArchivo/HeaderArchivo";
 import TablaArchivo from "../components/paginaArchivo/TablaArchivo";
