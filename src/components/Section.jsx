c666a29709ca7071b9cb76e4eae7154ada108f77
Diego Aldao <diego_ind@live.com.ar>
2023-04-16 11:39:55 -0300

    añadido un componente generico para las sections

---
+++ src/components/Section.jsx
@@ -0,0 +1,25 @@
+import React from "react";
+import styled from "styled-components";
+const Contenedor = styled.section`
+  padding: 60px 0px;
+  max-width: 1000px;
+  margin: 0 auto;
+  position: relative;
+  .destino {
+    position: absolute;
+    top: -50px;
+    visibility: 0;
+    opacity: 0;
+  }
+`;
+
+const Section = ({ id, children }) => {
+  return (
+    <Contenedor>
+      <div className="destino" id={id}></div>
+      {children}
+    </Contenedor>
+  );
+};
+
+export default Section;
