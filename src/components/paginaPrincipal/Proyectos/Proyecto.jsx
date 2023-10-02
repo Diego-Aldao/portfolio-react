175349576e09bd042da29281b3410279fc3d3854
Diego Aldao <diego_ind@live.com.ar>
2023-10-02 18:16:06 -0300

    añadidos estilos y un nuevo styled component para el titulo de los proyectos

--- src/components/paginaPrincipal/Proyectos/Proyecto.jsx
+++ src/components/paginaPrincipal/Proyectos/Proyecto.jsx
@@ -3,10 +3,30 @@ import styled, { css } from "styled-components";
 import { Icon } from "@iconify/react";
 import { Link } from "react-router-dom";
 
+const TituloProyecto = styled.header`
+  width: 100%;
+  h3 {
+    text-transform: capitalize;
+    font-size: clamp(22px, 4vw, 26px);
+    margin-bottom: 5px;
+  }
+  .subtitulo {
+    font-family: var(--fuente-mono);
+    text-transform: capitalize;
+    font-size: 13px;
+    color: var(--color-principal);
+  }
+`;
+
 const ImagenProyecto = styled.div`
   width: 100%;
   max-height: 350px;
   position: relative;
+  overflow: hidden;
+  border-radius: 5px;
+  img {
+    object-fit: fill;
+  }
   @media (min-width: 768px) {
     grid-row: -1;
     grid-column: ${({ imagen }) => (imagen ? "6 / 13" : "1 / 8")};
@@ -19,7 +39,11 @@ const ImagenProyecto = styled.div`
       top: 0px;
       width: 100%;
       height: 100%;
-      background: var(--color-principal);
+      background: linear-gradient(
+        to ${({ imagen }) => (imagen ? "left" : "right")},
+        #8135e576,
+        var(--color-bg-secundario)
+      );
       mix-blend-mode: multiply;
       transition: var(--transition);
     }
@@ -32,7 +56,8 @@ const ImagenProyecto = styled.div`
 const LinksProyecto = styled.div`
   display: flex;
   justify-content: flex-end;
-  margin-top: 20px;
+  margin-top: 10px;
+
   a {
     display: inline-block;
     text-align: center;
@@ -71,14 +96,16 @@ const LinksProyecto = styled.div`
 
   a:hover {
     color: var(--color-principal);
+    &:after {
+      background-color: var(--color-principal);
+    }
   }
   @media (min-width: 768px) {
     justify-content: ${({ imagen }) => (imagen ? "flex-start" : "flex-end")};
   }
 `;
 const DescripcionProyecto = styled.div`
-  background: var(--color-bg-principal);
-  margin-bottom: 10px;
+  background: var(--color-bg-secundario);
   p {
     font-size: 14px;
     font-family: var(--fuente-sans);
@@ -87,33 +114,31 @@ const DescripcionProyecto = styled.div`
     text-transform: capitalize;
   }
   @media (min-width: 768px) {
-    margin-bottom: 0px;
+    border-radius: 5px;
     padding: ${({ imagen }) =>
-      imagen ? "10px 5px 10px 0px" : "10px 0px 10px 5px"};
+      imagen ? "5px 5px 5px 0px" : "5px 0px 5px 5px"};
   }
 `;
 
 const ListaTecnologias = styled.ul`
-  background: var(--color-bg-principal);
-  padding: 0px 0px 10px;
   display: flex;
   justify-content: flex-start;
   flex-wrap: wrap;
-  margin-bottom: 10px;
+  gap: 5px 10px;
+  width: fit-content;
+  background: var(--color-bg-secundario);
   li {
-    padding: 0px 15px 5px 0px;
     color: var(--color-fuente-principal);
     font-family: var(--fuente-mono);
     text-transform: capitalize;
-    font-size: 14px;
+    font-size: 12px;
     font-weight: 500;
   }
   @media (min-width: 768px) {
+    margin-left: ${({ imagen }) => (imagen ? "0" : "auto")};
+
+    max-width: 60%;
     justify-content: ${({ imagen }) => (imagen ? "flex-start" : "flex-end")};
-    li {
-      padding: ${({ imagen }) =>
-        imagen ? "0px 15px 5px 0px" : " 0px 0px 5px 15px"};
-    }
   }
 `;
 
@@ -125,16 +150,18 @@ const Item = styled.div`
   max-width: 550px;
   margin: 0 auto 50px;
   position: relative;
+  border-radius: 5px;
+  overflow-x: hidden;
+  background: var(--color-bg-secundario);
   p,
   h3 {
     font-family: var(--fuente-sans);
   } //como el padre es un anchor, y los anchors tienen fuente mono, en este caso tengo que sobreescribir la fuente
   @media (min-width: 768px) {
     min-height: 280px;
-    max-width: 100%;
+    max-width: 900px;
     grid-template-columns: repeat(12, 1fr);
-    padding: ${({ imagen }) =>
-      imagen ? "0px 0px 0px 15px" : "0px 15px 0px 0px"};
+    padding: 0px;
   }
   @media (min-width: 1024px) {
     min-height: 320px;
@@ -145,7 +172,7 @@ const Item = styled.div`
       filter: brightness(100%);
     }
     &:after {
-      background: none;
+      opacity: 0;
     }
   }
   &:hover ${LinksProyecto} {
@@ -153,37 +180,25 @@ const Item = styled.div`
       width: 100%;
     }
   }
-  &:hover ${DescripcionProyecto}, &:hover ${ListaTecnologias}, &:hover {
-    background: var(--color-bg-secundario);
-  }
 `;
 
 const ContenidoProyecto = styled.div`
-  padding: 15px 0px;
+  padding: 10px 10px 20px;
+  margin-top: 10px;
   position: relative;
-  h3 {
-    text-transform: capitalize;
-    font-size: clamp(22px, 4vw, 26px);
-    margin-bottom: 10px;
-  }
-  .subtitulo {
-    font-family: var(--fuente-mono);
-    text-transform: capitalize;
-    font-size: 13px;
-    margin-bottom: 10px;
-    color: var(--color-principal);
-  }
+  display: flex;
+  flex-direction: column;
+  gap: 10px;
+
   @media (min-width: 768px) {
     display: flex;
     flex-direction: column;
     justify-content: center;
     grid-row: -1;
+    margin-top: 0px;
     grid-column: ${({ imagen }) => (imagen ? "1 / 9" : "5 / 13")};
     text-align: ${({ imagen }) => (imagen ? "start" : "end")};
-    padding: 0px;
-    .subtitulo {
-      margin-bottom: 0px;
-    }
+    padding: 20px 10px;
   }
   @media (min-width: 1080px) {
     grid-column: ${({ imagen }) => (imagen ? "1 / 8" : "6 / 13")};
@@ -214,11 +229,13 @@ const Proyecto = ({ data }) => {
     <Item imagen={imagenEnd}>
       <Especial to={links[0].url} target="_blank" />
       <ImagenProyecto imagen={imagenEnd}>
-        <img src={imagenUrl} alt="" />
+        <img src={imagenUrl} alt={`imagen del proyecto ${titulo}`} />
       </ImagenProyecto>
       <ContenidoProyecto imagen={imagenEnd}>
-        <h3>{titulo}</h3>
-        <p className="subtitulo">{tipo}</p>
+        <TituloProyecto>
+          <h3>{titulo}</h3>
+          <p className="subtitulo">{tipo}</p>
+        </TituloProyecto>
         <DescripcionProyecto imagen={imagenEnd}>
           <p>{descripcion}</p>
         </DescripcionProyecto>
